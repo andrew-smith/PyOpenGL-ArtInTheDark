944761c06e9872da985cb7de7d870bab558bd544
Andrew Smith <andrew@fisherboy.co.nz>
2012-10-04 11:40:02 +1300

    Inverted transparent circle function

--- GLCircles.py
+++ GLCircles.py
@@ -52,4 +52,29 @@ def glDrawTransparentCircle(radius, colourR, colourG, colourB):
     
     glEnd()
 
-
+def glDrawInvertedTransparentCircle(radius, colourR, colourG, colourB):
+    """
+    No colour should have been set before this function
+    """
+    
+    glBegin(GL_TRIANGLE_FAN)
+    
+    #set inside transparent colour
+    glColor4f(colourR, colourG, colourB, 0.0)
+    # first point is center
+    glVertex2f(0.0, 0.0)
+    
+    
+    #set outside non-transparent colour
+    glColor4f(colourR, colourG, colourB, 1.0)
+    angle = 0
+    while True:
+        rads = math.radians(angle)
+        glVertex2f(math.cos(rads) * radius, math.sin(rads) * radius)
+        angle += 0.5 # smoothness of circle (increase for rough circles)
+        if angle > 360:
+            break
+    
+    glEnd()
+    
+    
