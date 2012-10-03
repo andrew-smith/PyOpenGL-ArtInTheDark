bfe9b11e28e587f6c1da8faea3360ea7d52d52fb
Andrew Smith <andrew@fisherboy.co.nz>
2012-10-04 00:48:58 +1300

    New function to draw transparent circle

--- GLCircles.py
+++ GLCircles.py
@@ -26,5 +26,30 @@ def glDrawCircle(radius):
             break
     
     glEnd()
+    
+    
+    
+def glDrawTransparentCircle(radius, colourR, colourG, colourB):
+    """
+    Inside colour should have been set before this function
+    """
+    
+    glBegin(GL_TRIANGLE_FAN)
+    
+    # first point is center
+    glVertex2f(0.0, 0.0)
+    
+    #set outside transparent colour
+    glColor4f(colourR, colourG, colourB, 0.0)
+    
+    angle = 0
+    while True:
+        rads = math.radians(angle)
+        glVertex2f(math.cos(rads) * radius, math.sin(rads) * radius)
+        angle += 0.5 # smoothness of circle (increase for rough circles)
+        if angle > 360:
+            break
+    
+    glEnd()
 
 
